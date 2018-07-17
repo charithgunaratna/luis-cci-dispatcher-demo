a5e488569ac37ae4f5f1c7e17bd204ab1ecfe5fd
Charith Gunaratna <charithg@microsoft.com>
2018-07-17 11:03:34 -0700

    Initial commit

---
+++ AspNetCore-Luis-Dispatch-Bot/CCIIntentScorerClient.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspNetCore_Luis_Dispatch_Bot
+{
+    public class QueryIntentScore
+    {
+        public string IntentId { get; set; }
+        public double RankScore { get; set; }
+    }
+
+    public class CCIIntentScorerClient
+    {
+        private HttpClient _client;
+        private string _baseUri;
+
+        public CCIIntentScorerClient(string baseUri)
+        {
+            _baseUri = baseUri;
+            _client = new HttpClient();
+        }
+
+        public async Task<QueryIntentScore> GetIntentScoreAsync(string query)
+        {
+            var response = await _client.GetAsync(_baseUri + $"/api/v1/intentscore/getintentscoreasync?query={query}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var score = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<QueryIntentScore>(score);
+            }
+
+            return null;
+        }
+    }
+}
